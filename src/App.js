3e7b5e50fa8358b509f372c92ecffadaa90dab7a
Austin Dryden <austinisnorobot@gmail.com>
2020-02-20 10:36:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Taking Play Mountain(by Strategy)</p>
+      <img src="~/public/photos/noguchi-15.jpg"></img>
     </div>
   );
 }
