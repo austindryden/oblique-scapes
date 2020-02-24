c0239dbd42f4cc6039bbfe5098300f80cb4eb975
Austin Dryden <austinisnorobot@gmail.com>
2020-02-24 12:38:01 -0500

    updates readme

--- src/ObiApp.js
+++ src/ObiApp.js
@@ -14,9 +14,7 @@ export default class ObiApp extends React.Component{
     render(){
         return(<div>
             <button onClick={this._changeAbout}>{this.state.aboutMode ? "use this project" : "about this project" }</button><br />
-            <button style={{display:""}} onClick={this._changeColor}> {this.state.colorMode ? "Black and White" : "      Full Color      "}</button>
-            {/* <div style={{display:(this.state.aboutMode ? "":"none")}}><About colorMode={this.state.colorMode} />  </div>
-            <div style={{display:(this.state.aboutMode ? "none":"")}}><CardDeck colorMode={this.state.colorMode} /></div> */}
+            <button onClick={this._changeColor}> {this.state.colorMode ? "Black and White" : "      Full Color      "}</button>
             {this.state.aboutMode && <About handleClick={this._changeAbout} colorMode={this.state.colorMode} />}
             <CardDeck colorMode={this.state.colorMode} />
         </div>)
